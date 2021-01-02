51df48b2cd1c062b8f77808ed0cd3ef50996b860
sanjay.dhamelia <sanjay.dhamelia@stl.tech>
2021-01-02 12:50:06 +0530

    did some refactore in test case by adding @Before annotation on setup method

--- tdd/src/test/java/tdd/kata/handson/StringCalculatorTest.java
+++ tdd/src/test/java/tdd/kata/handson/StringCalculatorTest.java
@@ -2,22 +2,30 @@ package tdd.kata.handson;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class StringCalculatorTest {
 
+	private StringCalculator calculator;
+	
+	/**
+	 * Setup method block will be called before running of every test case.
+	 * i.e before @Test annotated method @Before will be called  
+	 */
+	@Before
+	public void setup() {
+		calculator = new StringCalculator();
+	}
 	
 	@Test
 	public void sumMustBeZeroForEmptyString() {
-		StringCalculator calculator = new StringCalculator();
 		int sum = calculator.add("");
 		assertEquals(0, sum);
 	}
 	
 	@Test
 	public void sumMustReturnSameNumberForNonEmptySingleValueString() {
-		StringCalculator calculator = new StringCalculator();
-		
 		int sum = calculator.add("10");
 		assertEquals(10, sum);
 		
@@ -27,12 +35,10 @@ public class StringCalculatorTest {
 	
 	@Test
 	public void sumMustReturnAdditionOfGivenNumberInInputString() {
-		StringCalculator calculator = new StringCalculator();
-		
 		int sum = calculator.add("10,5");
 		assertEquals(15, sum);
-		
-		sum = calculator.add("1,4");
-		assertEquals(5, sum);
+			
+		sum = calculator.add("1,2,3,4,5");
+		assertEquals(15, sum);
 	}
 }
