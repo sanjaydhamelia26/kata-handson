a9e31c07f5f5b58d743b4dc1ec28d142ccfc821b
sanjay.dhamelia <sanjay.dhamelia@stl.tech>
2021-01-09 19:16:28 +0530

    Scenarion 4 change implemenatation for the string which contains the delimeter itself

--- tdd/src/main/java/tdd/kata/handson/StringCalculator.java
+++ tdd/src/main/java/tdd/kata/handson/StringCalculator.java
@@ -9,8 +9,29 @@ public class StringCalculator {
 			return 0;
 		}
 		
-		int sum = Arrays.stream(input.split(",|\\n")).mapToInt(Integer::parseInt).sum();
+		String actulaInput = decideInputString(input);
+		int sum = Arrays.stream(actulaInput.split(decideSplitPattern(input))).mapToInt(Integer::parseInt).sum();
 		
 		return sum;
+	}
+	
+	private String decideSplitPattern(String input) {
+		String splitPattern = ",|\\n";
+		if (input.startsWith("//")) {
+			int indexOf = input.indexOf("\n");
+			String substring = input.substring(0, indexOf);
+			splitPattern = String.valueOf(substring.charAt(substring.length()-1));
+		}
+		
+		return splitPattern;
+	}
+	
+	private String decideInputString(String input) {
+		if (input.startsWith("//")) {
+			String[] inputs = input.split("\n");
+			// TODO Length Check
+			input = inputs[1];
+		}
+		return input;
 	}
 }
