a34e0e1b8ce3ff1e16eb91fb7023a686226e72b3
sanjay.dhamelia <sanjay.dhamelia@stl.tech>
2021-01-10 12:33:42 +0530

    Scenario 6: revert utility method calls and add logic of number to skip with value greater than 1000

--- tdd/src/main/java/tdd/kata/handson/StringCalculator.java
+++ tdd/src/main/java/tdd/kata/handson/StringCalculator.java
@@ -4,9 +4,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import tdd.kata.handson.util.NumberUtils;
-import tdd.kata.handson.util.StringUtils;
-
 public class StringCalculator {
 
 	public int add(String input) {
@@ -14,14 +11,51 @@ public class StringCalculator {
 			return 0;
 		}
 		
-		String actulaInput = StringUtils.decideInputString(input);
-		String splitPattern = StringUtils.decideSplitPattern(input);
+		String actulaInput = decideInputString(input);
+		String splitPattern = decideSplitPattern(input);
 		
 		List<Integer> inputNumbers = Arrays.stream(actulaInput.split(splitPattern))
 										.map(Integer::parseInt).collect(Collectors.toList());
 		
-		NumberUtils.validateNumbers(inputNumbers);
+		validateNumbers(inputNumbers);
 		
-		return NumberUtils.addNumbers(inputNumbers);
+		return addNumbers(inputNumbers);
+	}
+	
+	public int addNumbers(List<Integer> inputNumbers) {
+		int sum = 0;
+		for (Integer number : inputNumbers) {
+			sum += number < 1000 ? number : 0;
+		}
+		return sum;
+	}
+
+	public void validateNumbers(List<Integer> inputNumbers) {
+		List<Integer> negativeNumberList = inputNumbers.stream().filter(n -> n < 0).collect(Collectors.toList());
+		
+		if (negativeNumberList.size() > 0) {
+			String negativeNumber = negativeNumberList.stream().map(String::valueOf).collect(Collectors.joining(","));
+			throw new IllegalArgumentException("Negative Number: " + negativeNumber + " not allowed");
+		}
+	}
+	
+	public String decideSplitPattern(String input) {
+		String splitPattern = ",|\\n";
+		if (input.startsWith("//")) {
+			int indexOf = input.indexOf("\n");
+			String substring = input.substring(0, indexOf);
+			splitPattern = String.valueOf(substring.charAt(substring.length()-1));
+		}
+		
+		return splitPattern;
+	}
+	
+	public String decideInputString(String input) {
+		if (input.startsWith("//")) {
+			String[] inputs = input.split("\n");
+			// TODO Length Check
+			input = inputs[1];
+		}
+		return input;
 	}
 }
