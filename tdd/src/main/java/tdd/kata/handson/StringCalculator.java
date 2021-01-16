b86e783bc666b37815fa03060751912fd0a7fe54
sanjay.dhamelia <sanjay.dhamelia@stl.tech>
2021-01-16 21:00:31 +0530

    Scenario 8, added logic for the multiple delimeters by changing the existing logic

--- tdd/src/main/java/tdd/kata/handson/StringCalculator.java
+++ tdd/src/main/java/tdd/kata/handson/StringCalculator.java
@@ -1,7 +1,9 @@
 package tdd.kata.handson;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class StringCalculator {
@@ -42,17 +44,40 @@ public class StringCalculator {
 	public String decideSplitPattern(String input) {
 		String splitPattern = ",|\\n";
 		if (input.startsWith("//")) {
-			int indexOf = input.indexOf("\n");
-			String delimeter = identifyDelimeter(input, indexOf);
-			String multiDelimeterPattern = "\\Q"+delimeter+"\\E";
-			splitPattern = multiDelimeterPattern;
+			String[] inputs = input.split("\n");
+			if (inputs.length > 1) {
+				splitPattern = identifyDelimeter(inputs[0]);
+			}
 		}
 		
 		return splitPattern;
 	}
 
-	private String identifyDelimeter(String input, int indexOf) {
-		return input.substring(0, indexOf).replace("[", "").replace("]", "").replace("//", "");
+	private String identifyDelimeter(String input) {
+		int delimeterStartPosition = input.indexOf("//")+2;
+		
+		List<String> listOfDelimeters = new ArrayList<>();
+		int startPosOfMutipleDelimeter = 0;
+		
+		for (int i = delimeterStartPosition; i < input.length(); i++) {
+			if (input.charAt(i) == '[') {
+				startPosOfMutipleDelimeter = i + 1;
+				while(input.charAt(++i) != ']');
+				listOfDelimeters.add(input.substring(startPosOfMutipleDelimeter,i));
+			} else {
+				listOfDelimeters.add(String.valueOf(input.charAt(i)));
+			}
+		}
+		
+		StringBuilder regex = new StringBuilder();
+		for(int i= 0; i < listOfDelimeters.size(); i++) {
+			regex.append(Pattern.quote(listOfDelimeters.get(i)));
+			if(i<listOfDelimeters.size()-1) {
+				regex.append("|");
+			}
+		}
+		
+		return regex.toString();
 	}
 	
 	public String decideInputString(String input) {
